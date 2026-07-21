39de1997e1e0faf56cfbf8f902f397a7dfabb336
kanyeezyking <angusshugg0@gmail.com>
2026-07-22 03:58:00 +1000

    Update nbt-exporter.js

--- js/nbt-exporter.js
+++ js/nbt-exporter.js
@@ -1,1 +1,121 @@
+function exportNBT(){
 
+
+    let structure =
+    getVoxelStructure();
+
+
+
+    if(!structure){
+
+
+        alert(
+        "Nothing has been generated yet."
+        );
+
+
+        return;
+
+    }
+
+
+
+    let nbtData = {
+
+
+        name:
+        structure.name,
+
+
+        size:
+        structure.size,
+
+
+        mode:
+        structure.mode,
+
+
+        blocks:
+        structure.voxels.map(voxel=>{
+
+
+            return {
+
+
+                pos:[
+
+                    voxel.x,
+
+                    voxel.y,
+
+                    voxel.z
+
+                ],
+
+
+                state:
+                voxel.block
+
+
+            };
+
+
+        })
+
+
+    };
+
+
+
+    downloadNBT(
+        nbtData
+    );
+
+}
+
+
+
+
+
+function downloadNBT(data){
+
+
+    let file =
+    JSON.stringify(
+        data,
+        null,
+        2
+    );
+
+
+
+    let blob =
+    new Blob(
+        [file],
+        {
+            type:
+            "application/json"
+        }
+    );
+
+
+
+    let link =
+    document.createElement("a");
+
+
+
+    link.href =
+    URL.createObjectURL(blob);
+
+
+
+    link.download =
+    "voxelforge-structure.nbt";
+
+
+
+    link.click();
+
+
+}

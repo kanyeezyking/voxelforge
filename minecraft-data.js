059a8f272b6ea1827d72682cf15c399dd49fc8a4
kanyeezyking <angusshugg0@gmail.com>
2026-07-22 00:55:00 +1000

    Create minecraft-data.js

---
+++ minecraft-data.js
@@ -0,0 +1,79 @@
+const minecraftBlocks = [
+
+{
+name:"Grass Block",
+icon:"🟩",
+type:"block"
+},
+
+{
+name:"Stone",
+icon:"⬜",
+type:"block"
+},
+
+{
+name:"Dirt",
+icon:"🟫",
+type:"block"
+},
+
+{
+name:"Oak Log",
+icon:"🪵",
+type:"block"
+},
+
+{
+name:"Diamond Block",
+icon:"💎",
+type:"block"
+},
+
+{
+name:"Crafting Table",
+icon:"🛠️",
+type:"block"
+}
+
+];
+
+
+const minecraftMobs = [
+
+{
+name:"Creeper",
+icon:"👾",
+type:"mob"
+},
+
+{
+name:"Zombie",
+icon:"🧟",
+type:"mob"
+},
+
+{
+name:"Pig",
+icon:"🐷",
+type:"mob"
+}
+
+];
+
+
+const minecraftEntities = [
+
+{
+name:"Boat",
+icon:"🚣",
+type:"entity"
+},
+
+{
+name:"Minecart",
+icon:"🚋",
+type:"entity"
+}
+
+];
